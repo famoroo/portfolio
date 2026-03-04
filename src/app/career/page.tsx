36830adfc532381f74f25366dd0acf9d971a3e7b
arakawa-admin <arakawa@amarc.co.jp>
2026-03-04 10:17:49 +0900

    fix: responsive on career page

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,7 +1,95 @@
-import ClientPageForCareers from "./clientPage";
+"use client";
+import {
+    Timeline,
+    TimelineItem,
+    TimelineSeparator,
+    TimelineConnector,
+    TimelineContent,
+    TimelineDot,
+    TimelineOppositeContent
+} from '@mui/lab';
 
-export default async function PageForCareers() {
+import { timelineOppositeContentClasses } from '@mui/lab/TimelineOppositeContent';
+
+import {
+    Container,
+    Typography,
+    Stack,
+    useMediaQuery
+} from '@mui/material';
+import PageTitle from "@/components/PageTitle";
+
+import ListContentForCareer from "./ListContent";
+import GridContentForCareer from "./GridContent";
+
+import { careers } from "@/constants/careers";
+
+export default function PageForCareers() {
+    const isMobile = useMediaQuery("(max-width: 600px)");
+
     return (
-        <ClientPageForCareers />
+        <Container
+            maxWidth="lg"
+            sx={{ paddingTop: "100px" }}
+            >
+            <PageTitle
+                title="career"
+                subtitle="やってきたこと"
+                />
+
+            {isMobile
+            ? (
+                <Stack spacing={2}>
+                    {careers.map((career) => (
+                        <div key={career.year}>
+                        {career.item
+                            ?
+                            <ListContentForCareer item={career.item} year={career.year} />
+                            :
+                            <GridContentForCareer career={career} year={career.year} />
+                        }
+                        </div>
+                    ))}
+                </Stack>
+            ) : (
+                <Timeline
+                    sx={{
+                        [`& .${timelineOppositeContentClasses.root}`]: {
+                            flex: 0.1,
+                        },
+                    }}
+                    >
+                    {
+                        careers.map((career) => (
+                            <TimelineItem key={career.year}>
+                                <TimelineOppositeContent>
+                                    <Typography
+                                        color="success"
+                                        sx={{
+                                            fontWeight: "bold",
+                                            fontSize: "1.5rem",
+                                        }}
+                                        >
+                                        {career.year}年
+                                    </Typography>
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot color="success" />
+                                    <TimelineConnector sx={{ bgcolor: 'success.light' }}/>
+                                </TimelineSeparator>
+                                <TimelineContent sx={{ my: 2 }}>
+                                    {career.item
+                                        ?
+                                        <ListContentForCareer item={career.item} />
+                                        :
+                                        <GridContentForCareer career={career} />
+                                    }
+                                </TimelineContent>
+                            </TimelineItem>
+                        ))
+                    }
+                </Timeline>
+            )}
+        </Container>
     );
 }
